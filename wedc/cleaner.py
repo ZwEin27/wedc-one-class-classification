01003c4f0fb64e46aa9e3bd264a7de45f9b8b5c4
ZwEin <zwein27@gmail.com>
2016-08-09 14:01:12 -0700

    add common and functions

--- wedc/cleaner.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: ZwEin
-# @Date:   2016-08-09 13:47:28
-# @Last Modified by:   ZwEin
-# @Last Modified time: 2016-08-09 13:48:01
-
-
-
---
+++ wedc/common/cleaner.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author: ZwEin
+# @Date:   2016-08-09 13:50:35
+# @Last Modified by:   ZwEin
+# @Last Modified time: 2016-08-09 13:54:07
+
+
+class Cleaner(object):
+
+    @staticmethod
+    def clean(content):
+        pass
+
+
+
