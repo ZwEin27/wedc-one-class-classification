d4ec365c1babe92656c462f19865da43088c4cc1
ZwEin <zwein27@gmail.com>
2016-08-09 12:13:24 -0700

    add load_data()

---
+++ wedc/classifier.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: ZwEin
+# @Date:   2016-08-09 11:36:55
+# @Last Modified by:   ZwEin
+# @Last Modified time: 2016-08-09 11:42:24
---
+++ wedc/loader.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# @Author: ZwEin
+# @Date:   2016-08-08 11:46:11
+# @Last Modified by:   ZwEin
+# @Last Modified time: 2016-08-09 12:13:09
+
+
+import os
+import csv
+
+DC_DATA_FILE_FORMAT_JSON = 'json'
+DC_DATA_FILE_FORMAT_CSV = 'csv'
+
+DC_DATA_FILE_FORMATS = [
+    DC_DATA_FILE_FORMAT_JSON,
+    DC_DATA_FILE_FORMAT_CSV
+]
+
+
+DC_DEFAULT_TRAINING_DATA_FILEPATH = os.path.join(os.path.dirname(__file__), 'res', 'training_data.csv')
+
+
+class Loader(object):
+
+    #################################################
+    # Basic Load Data Functions
+    #################################################
+
+    def __load_data_jsonlines(path):
+        import jsonlines
+        dataset = []
+        lines = jsonlines.open(path, mode='r')
+        for line in lines:
+            dataset.append([line['sid'], line['content']])
+        return dataset
+
+    def __load_data_csv(path):
+        import csv
+        dataset = []
+        with open(path, 'rb') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                dataset.append(row)
+        return dataset
+
+    __loader_data_funcs = {
+        DC_DATA_FILE_FORMAT_JSON: __load_data_jsonlines,
+        DC_DATA_FILE_FORMAT_CSV: __load_data_csv
+    }
+
+    @staticmethod
+    def load_data(path, format='jsonlines'):
+        """
+        basic interface to load data
+        
+        """
+        return Loader.__loader_data_funcs[format](path)
+
+    #################################################
+    # Main Load Data Functions
+    #################################################
+    
+    @staticmethod
+    def load_training_data(filepath=DC_DEFAULT_TRAINING_DATA_FILEPATH):
+        return Loader.load_data(filepath, format='csv')
+       
+
+print Loader.load_training_data()
+
+
+
+
